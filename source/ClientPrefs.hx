4b1a727fe23dcb41583dfb31d0ffb63f8d5d1227
nelifs <paaalmachka@gmail.com>
2022-05-01 16:14:17 +0300

    i want pizza

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -142,6 +142,7 @@ class ClientPrefs {
 		FlxG.save.data.showcaseMode = showcaseMode;
 		FlxG.save.data.removePerfects = removePerfects;
 		FlxG.save.data.perfectWindow = perfectWindow;
+		FlxG.save.data.sickWindow = sickWindow;
 		FlxG.save.data.goodWindow = goodWindow;
 		FlxG.save.data.badWindow = badWindow;
 		FlxG.save.data.safeFrames = safeFrames;
