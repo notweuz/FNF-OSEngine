80257dcfe6e2eefdcc09c61cc7fe2bf9f404d6f8
not.weuz <weuz.main@gmail.com>
2022-05-01 20:13:02 +0400

    changed 1 number

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var osEngineVesrion:String = '1.0.0'; //This is also used for Discord RPC
+	public static var osEngineVesrion:String = '1.1.0'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
