d47408a20ac71dd251268a1f431652074f2a0566
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-05-01 22:33:21 -0300

    update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -29,7 +29,7 @@ using StringTools;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Note Colors', 'Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay', 'Chart Editor'];
+	var options:Array<String> = ['Note Colors', 'Controls' #if android, 'Android Controls' #end, 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay', 'Chart Editor'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
@@ -50,6 +50,10 @@ class OptionsState extends MusicBeatState
 				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
 			case 'Chart Editor':
 				openSubState(new options.ChartEditorSettingsSubState());
+			case 'Android Controls':
+			  #if android
+			  MusicBeatState.switchState(new android.AndroidControlsMenu());
+			  #end
 		}
 	}
 
@@ -90,7 +94,11 @@ class OptionsState extends MusicBeatState
 
 		changeSelection();
 		ClientPrefs.saveSettings();
-
+		
+    #if android
+  	addVirtualPad(UP_DOWN, A_B);
+    #end
+    
 		super.create();
 	}
 
