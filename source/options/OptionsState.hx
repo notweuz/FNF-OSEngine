66756c503fe9b536ac0570269a207231d812ecd0
pasta870 <96292419+pasta870@users.noreply.github.com>
2022-05-18 07:43:44 +0800

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -38,27 +38,27 @@ class OptionsState extends MusicBeatState
 		switch(label) {
 			case 'Note Colors':
 				#if android
-				removeVirtualPad();
+				addVirtualPad(FULL, A_B_C);
 				#end
 				openSubState(new options.NotesSubState());
 			case 'Controls':
 				#if android
-				removeVirtualPad();
+				addVirtualPad(FULL, A_B_C);
 				#end
 				openSubState(new options.ControlsSubState());
 			case 'Graphics':
 				#if android
-				removeVirtualPad();
+				addVirtualPad(FULL, A_B_C);
 				#end
 				openSubState(new options.GraphicsSettingsSubState());
 			case 'Visuals and UI':
 				#if android
-				removeVirtualPad();
+				addVirtualPad(FULL, A_B_C);
 				#end
 				openSubState(new options.VisualsUISubState());
 			case 'Gameplay':
 				#if android
-				removeVirtualPad();
+				addVirtualPad(FULL, A_B_C);
 				#end
 				openSubState(new options.GameplaySettingsSubState());
 			case 'Adjust Delay and Combo':
